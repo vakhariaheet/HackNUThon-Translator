b7d36de174108e226b45a45a1a120988639cecb6
Heet Vakharia <heetkv@gmail.com>
2023-04-30 12:05:56 +0530

    Initial Commit

--- src/utils/CurrencyConversion.ts
+++ src/utils/CurrencyConversion.ts
@@ -19,13 +19,12 @@ interface Info {
   timestamp: number;
 }
 export const convertCurrency = async (amt: string, from: string, to: string) => {
-    const headers = new Headers();
+
     const amount = amt.match(/\d+/g);
     if (!amount) throw new Error('Invalid amount');
-    headers.append('apiKey', 'xfoGCReVNkQYZz7IjeRr1xjyoy6eMtmy');
     const response = await axios.get<CurrencyResp>(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount[ 0 ]}`, {
         headers: {
-            apiKey: 'xfoGCReVNkQYZz7IjeRr1xjyoy6eMtmy'
+            apiKey: process.env.CURRENCY_API_KEY,
         }
     });
     return `${response.data.result} ${to}`;
